dac306507604f0bad673812c4ac3e7b30665b51a
Brian Luu <briluu@gmail.com>
2017-05-12 00:43:07 -0700

    populates rows dynamically

--- dashboard/js/superusers.js
+++ dashboard/js/superusers.js
@@ -6,11 +6,24 @@ $(function() {
 
     //displays list of all devices
     function getAllDevices() {
-        $.get(GET_ALL_DEVICES, function(data) {
-            console.log(data);
-        }).then(function(response) {
-
-        });
+        // $.get(GET_ALL_DEVICES, function(data) {
+        //     console.log(data);
+        // }).then(function(response) {
+        //     var data = [{"serialNum":"15348","status":"Active","city":"Nairobi","country":"Kenya","deploymentDate":"2017-04-03","mostRecentCycle":null},{"serialNum":"15345","status":"Active","city":"Mombasa","country":"Kenya","deploymentDate":"2017-04-03","mostRecentCycle":null},{"serialNum":"15345","status":"Active","city":"Nairobi","country":"Kenya","deploymentDate":"2017-01-03","mostRecentCycle":null},{"serialNum":"15344","status":"Active","city":"Wote","country":"Kenya","deploymentDate":"2017-04-03","mostRecentCycle":null},{"serialNum":"15344","status":"Active","city":"Nairobi","country":"Kenya","deploymentDate":"2017-01-03","mostRecentCycle":null},{"serialNum":"15342","status":"Active","city":"Dadaab","country":"Kenya","deploymentDate":"2017-04-03","mostRecentCycle":null},{"serialNum":"15347","status":"Active","city":"Port-au-Prince","country":"Haiti","deploymentDate":"2017-04-03","mostRecentCycle":null},{"serialNum":"15340","status":"Active","city":"Cercadie","country":"Haiti","deploymentDate":"2017-04-03","mostRecentCycle":null}];
+        //     console.log(data);
+        // });
+        var data = [{"serialNum":"15348","status":"Active","city":"Nairobi","country":"Kenya","deploymentDate":"2017-04-03","mostRecentCycle":null},{"serialNum":"15345","status":"Active","city":"Mombasa","country":"Kenya","deploymentDate":"2017-04-03","mostRecentCycle":null},{"serialNum":"15345","status":"Active","city":"Nairobi","country":"Kenya","deploymentDate":"2017-01-03","mostRecentCycle":null},{"serialNum":"15344","status":"Active","city":"Wote","country":"Kenya","deploymentDate":"2017-04-03","mostRecentCycle":null},{"serialNum":"15344","status":"Active","city":"Nairobi","country":"Kenya","deploymentDate":"2017-01-03","mostRecentCycle":null},{"serialNum":"15342","status":"Active","city":"Dadaab","country":"Kenya","deploymentDate":"2017-04-03","mostRecentCycle":null},{"serialNum":"15347","status":"Active","city":"Port-au-Prince","country":"Haiti","deploymentDate":"2017-04-03","mostRecentCycle":null},{"serialNum":"15340","status":"Active","city":"Cercadie","country":"Haiti","deploymentDate":"2017-04-03","mostRecentCycle":null}];
+        var table = $('.table');
+        for (var i = 0; i < data.length; i++) {
+            var tr = $('<tr></tr>');
+            table.append(tr);
+            tr.append($('<td>' + data[i].status + '</td>'));
+            tr.append($('<td>' + data[i].serialNum + '</td>'));
+            tr.append($('<td>' + data[i].mostRecentCycle + '</td>'));
+            tr.append($('<td>' + data[i].deploymentDate + '</td>'));
+            tr.append($('<td>' + data[i].city + '</td>'));
+            tr.append($('<td>' + data[i].country + '</td>'));
+        }
     }
 
 
@@ -22,6 +35,6 @@ $(function() {
         });
     }
 
-    // getAllDevices();
+    getAllDevices();
     // getSpecificDevice();
 });
