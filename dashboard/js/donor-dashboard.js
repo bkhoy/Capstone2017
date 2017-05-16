dafe986ada39883ad7da37412c36932628eb99d5
Brian Luu <briluu@gmail.com>
2017-05-15 19:04:54 -0700

    added comments to the code

--- dashboard/js/donor-dashboard.js
+++ dashboard/js/donor-dashboard.js
@@ -4,17 +4,117 @@
 
 'use strict';
 
+// global variables
+
+// holds json data of all devices
 var devices = [];
+// global variable for the google map
 var map = null;
+// dictionary for serial numbers and latitude/longitude coordinates
+// example item: 15348: [18.5944, -72.3074]
 var serialToLatLng = {};
 
-//Same thing as document.ready
+// runs this block of code when the page is ready/loaded
 $(function() {
 
     initChart();
 
+    // initializes the chart. uses Chart.js library.
+    function initChart() {
+        var ctx = document.getElementById("myChart").getContext("2d");
+        ctx.canvas.width = 800;
+        ctx.canvas.height = 600;
+
+        const colors = {
+            green: {
+                fill: '#e0eadf',
+                stroke: '#5eb84d',
+            },
+            lightBlue: {
+                stroke: '#6fccdd',
+            },
+            darkBlue: {
+                fill: '#92bed2',
+                stroke: '#3282bf',
+            },
+            purple: {
+                fill: '#8fa8c8',
+                stroke: '#75539e',
+            },
+        };
+
+        var response = {"15348":[],"15340":[{"startDateTime":"2017-04-14 09:35:00","0":"2017-04-14 09:35:00","totalChlorineProduced":"2089.33","1":"2089.33"},{"startDateTime":"2017-04-14 09:35:00","0":"2017-04-14 09:35:00","totalChlorineProduced":"2089.33","1":"2089.33"},{"startDateTime":"2017-04-14 09:35:00","0":"2017-04-14 09:35:00","totalChlorineProduced":"2089.33","1":"2089.33"},{"startDateTime":"2017-04-14 09:35:00","0":"2017-04-14 09:35:00","totalChlorineProduced":"2089.33","1":"2089.33"},{"startDateTime":"2017-04-14 09:35:00","0":"2017-04-14 09:35:00","totalChlorineProduced":"2089.33","1":"2089.33"}],"15342":[{"startDateTime":"2017-04-14 09:35:00","0":"2017-04-14 09:35:00","totalChlorineProduced":"36.8333","1":"36.8333"},{"startDateTime":"2017-04-14 09:35:00","0":"2017-04-14 09:35:00","totalChlorineProduced":"36.8333","1":"36.8333"},{"startDateTime":"2017-04-14 09:35:00","0":"2017-04-14 09:35:00","totalChlorineProduced":"36.8333","1":"36.8333"},{"startDateTime":"2017-04-14 09:35:00","0":"2017-04-14 09:35:00","totalChlorineProduced":"0","1":"0"},{"startDateTime":"2017-04-14 09:35:00","0":"2017-04-14 09:35:00","totalChlorineProduced":"0","1":"0"},{"startDateTime":"2017-04-14 09:35:00","0":"2017-04-14 09:35:00","totalChlorineProduced":"0","1":"0"},{"startDateTime":"2017-04-14 09:35:00","0":"2017-04-14 09:35:00","totalChlorineProduced":"0","1":"0"},{"startDateTime":"2017-04-14 09:35:00","0":"2017-04-14 09:35:00","totalChlorineProduced":"0","1":"0"},{"startDateTime":"2017-04-14 09:35:00","0":"2017-04-14 09:35:00","totalChlorineProduced":"0","1":"0"},{"startDateTime":"2017-04-14 09:35:00","0":"2017-04-14 09:35:00","totalChlorineProduced":"0","1":"0"}]};
+        console.log(response);
+
+        var data = {
+            datasets: [{
+                label: "Total Chlorine Production Rate",
+                fill: false,
+                lineTension: 0.0,
+                backgroundColor: "rgba(75,192,192,0.4)", // The fill color under the line
+                borderColor: "rgba(75,192,192,1)", // 	The color of the line
+                borderCapStyle: 'round',
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBackgroundColor: "#fff",
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                spanGaps: false, // If true, lines will be drawn between points with no or null data
+                data: [65, 59, 80, 81, 74, 77, 85],  // data points go here
+            }, {
+                label: "One device's chlorine rate",
+                fill: false,
+                lineTension: 0.0,
+                backgroundColor: colors.green.fill, // The fill color under the line
+                borderColor: colors.green.stroke, // 	The color of the line
+                borderCapStyle: 'round',
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBackgroundColor: colors.green.stroke,
+                pointBorderWidth: 1,
+                pointHighlightStroke: colors.green.stroke,
+                pointHoverRadius: 5,
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                spanGaps: false, // If true, lines will be drawn between points with no or null data
+                data: [20, 39, 40, 11, 24, 20, 42]  // data points go here
+            }
+            ],
+            xLabels: ['Apr 8', 'Apr 9', 'Apr 10', 'Apr 11', 'Apr 12', 'Apr 13', 'Apr 14'],
+            yLabels: ['testing']
+        };
+        var options = {
+            title: {
+                display: true,
+                text: "Your devices at work",
+                fontSize: 40,
+                padding: 60,
+                fontFamily: "'Source Sans Pro', 'sans-serif'",
+                fontStyle: 'normal'
+            },
+            maintainAspectRatio: true
+            // if you want stacked line charts, uncomment
+            // scales: {
+            //     yAxes: [{
+            //         stacked: true
+            //     }]
+            // }
+        };
+        var myLineChart = new Chart(ctx, {
+            type: 'line',
+            data: data,
+            options: options
+        });
+    }
+
 });
 
+// initiatives the list of devices on the left side of the map
+// requires that the devices list is populated (meaning a successful call to the server)
 function initDevices() {
     var parent = $("#scrollable-div");
     for (var i = 0; i < devices.length; i++) {
@@ -51,11 +151,19 @@ function initMap() {
         },
         error: function(error) {
             console.log(error);
+
         }
     });
+    // To run this code locally (with hard coded data below):
+    // 1. comment the above ajax call
+    // 2. uncomment the below functions
+    // 3. uncomment the data variable (fake data) in populateMap
+    // populateMap(null);
+    // initDevices();
 }
 
-function populateMap() {
+// initializes and populates the Google Map with the markers
+function populateMap(data) {
     var kenya = {lat: 0.0236, lng: 37.9062};
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 7,
@@ -63,7 +171,8 @@ function populateMap() {
         scrollwheel: false
     });
 
-    var data = [{"serialNum":"15340","city":"Cercadie","country":"Haiti","latitude":"18.5944","longitude":"-72.3074","deploymentDate":"2017-04-03"},{"serialNum":"15342","city":"Dadaab","country":"Kenya","latitude":"0.0925798","longitude":"40.3191","deploymentDate":"2017-04-03"},{"serialNum":"15344","city":"Wote","country":"Kenya","latitude":"-1.78863","longitude":"37.6333","deploymentDate":"2017-04-03"},{"serialNum":"15345","city":"Mombasa","country":"Kenya","latitude":"-4.04348","longitude":"39.6682","deploymentDate":"2017-04-03"},{"serialNum":"15347","city":"Port-au-Prince","country":"Haiti","latitude":"19.304","longitude":"-72.0379","deploymentDate":"2017-04-03"},{"serialNum":"15348","city":"Nairobi","country":"Kenya","latitude":"-1.29207","longitude":"36.8219","deploymentDate":"2017-04-03"}];
+    // uncomment if you want to use manual data
+    // var data = [{"serialNum":"15340","city":"Cercadie","country":"Haiti","latitude":"18.5944","longitude":"-72.3074","deploymentDate":"2017-04-03"},{"serialNum":"15342","city":"Dadaab","country":"Kenya","latitude":"0.0925798","longitude":"40.3191","deploymentDate":"2017-04-03"},{"serialNum":"15344","city":"Wote","country":"Kenya","latitude":"-1.78863","longitude":"37.6333","deploymentDate":"2017-04-03"},{"serialNum":"15345","city":"Mombasa","country":"Kenya","latitude":"-4.04348","longitude":"39.6682","deploymentDate":"2017-04-03"},{"serialNum":"15347","city":"Port-au-Prince","country":"Haiti","latitude":"19.304","longitude":"-72.0379","deploymentDate":"2017-04-03"},{"serialNum":"15348","city":"Nairobi","country":"Kenya","latitude":"-1.29207","longitude":"36.8219","deploymentDate":"2017-04-03"}];
 
     for (var i = 0; i < data.length; i++) {
         var device = data[i];
@@ -79,51 +188,4 @@ function populateMap() {
         // add the marker to the map
         marker.setMap(map);
     }
-}
-
-function initChart() {
-    var ctx = document.getElementById("myChart").getContext("2d");
-    ctx.canvas.width = 800;
-    ctx.canvas.height = 600;
-    var data = {
-        //labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [
-            {
-                label: "Chlorine Production Rate",
-                fill: true,
-                lineTension: 0.0,
-                backgroundColor: "rgba(75,192,192,0.4)", // The fill color under the line
-                borderColor: "rgba(75,192,192,1)", // 	The color of the line
-                borderCapStyle: 'round',
-                borderDashOffset: 0.0,
-                borderJoinStyle: 'miter',
-                pointBackgroundColor: "#fff",
-                pointBorderWidth: 1,
-                pointHoverRadius: 5,
-                pointHoverBorderWidth: 2,
-                pointRadius: 1,
-                pointHitRadius: 10,
-                data: [65, 59, 80, 81, 74, 77, 85], // data points go here
-                spanGaps: false // If true, lines will be drawn between points with no or null data
-            }
-        ],
-        xLabels: ['Apr 8', 'Apr 9', 'Apr 10', 'Apr 11', 'Apr 12', 'Apr 13', 'Apr 14'],
-        yLabels: ['testing']
-    };
-    var options = {
-        title: {
-            display: true,
-            text: "Your devices at work",
-            fontSize: 40,
-            padding: 60,
-            fontFamily: "'Source Sans Pro', 'sans-serif'",
-            fontStyle: 'normal'
-        },
-        maintainAspectRatio: false
-    };
-    var myLineChart = new Chart(ctx, {
-        type: 'line',
-        data: data,
-        options: options
-    });
 }
