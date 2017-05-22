8106fa1db90273d850bccad53d96ed47bb355484
Brian Luu <briluu@gmail.com>
2017-05-21 17:45:46 -0700

    fixed an error

--- dashboard/js/superusers.js
+++ dashboard/js/superusers.js
@@ -5,7 +5,7 @@ $(function() {
     const GET_DEVICE_DETAILS = "http://boneappletea.me/webservice/getDeviceInfo.php?serialNum=15348";
 
     //displays list of all devices
-    function getAllDevices(data) {
+    function getAllDevices() {
         $.ajax({
             url: GET_ALL_DEVICES,
             dataType: "json",
