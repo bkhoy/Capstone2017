78f6dc7b11af318a151aaae78890326be3164853
Brian Luu <briluu@gmail.com>
2017-05-21 17:45:13 -0700

    added DataTables to the overview page

--- dashboard/js/superusers.js
+++ dashboard/js/superusers.js
@@ -12,19 +12,24 @@ $(function() {
             method: "GET",
             success: function(data) {
                 // will need to uncomment this for local dev (CORS issue will come up)
-                // var data = [{"serialNum":"15348","status":"Active","city":"Nairobi","country":"Kenya","deploymentDate":"2017-04-03","mostRecentCycle":null},{"serialNum":"15345","status":"Active","city":"Mombasa","country":"Kenya","deploymentDate":"2017-04-03","mostRecentCycle":null},{"serialNum":"15345","status":"Active","city":"Nairobi","country":"Kenya","deploymentDate":"2017-01-03","mostRecentCycle":null},{"serialNum":"15344","status":"Active","city":"Wote","country":"Kenya","deploymentDate":"2017-04-03","mostRecentCycle":null},{"serialNum":"15344","status":"Active","city":"Nairobi","country":"Kenya","deploymentDate":"2017-01-03","mostRecentCycle":null},{"serialNum":"15342","status":"Active","city":"Dadaab","country":"Kenya","deploymentDate":"2017-04-03","mostRecentCycle":null},{"serialNum":"15347","status":"Active","city":"Port-au-Prince","country":"Haiti","deploymentDate":"2017-04-03","mostRecentCycle":null},{"serialNum":"15340","status":"Active","city":"Cercadie","country":"Haiti","deploymentDate":"2017-04-03","mostRecentCycle":null}];
+                //var data = [{"serialNum":"15348","status":"Active","city":"Nairobi","country":"Kenya","placementDate":"2017-04-03","mostRecentCycle":"2017-04-17 09:35:00"},{"serialNum":"15345","status":"Active","city":"Mombasa","country":"Kenya","placementDate":"2017-04-03","mostRecentCycle":"2017-04-17 09:35:00"},{"serialNum":"15344","status":"Active","city":"Wote","country":"Kenya","placementDate":"2017-04-03","mostRecentCycle":"2017-04-17 09:35:00"},{"serialNum":"15342","status":"Active","city":"Dadaab","country":"Kenya","placementDate":"2017-04-03","mostRecentCycle":"2017-04-17 09:35:00"},{"serialNum":"15347","status":"Active","city":"Port-au-Prince","country":"Haiti","placementDate":"2017-04-03","mostRecentCycle":"2017-04-17 09:35:00"},{"serialNum":"15340","status":"Active","city":"Cercadie","country":"Haiti","placementDate":"2017-04-03","mostRecentCycle":"2017-04-17 09:35:00"}];
 
                 var table = $('.table');
                 for (var i = 0; i < data.length; i++) {
                     var tr = $('<tr></tr>');
                     table.append(tr);
-                    tr.append($('<td>' + data[i].status + '</td>'));
+                    $(tr).append("<td><img src='img/active.svg'</td>");
                     tr.append($('<td>' + data[i].serialNum + '</td>'));
                     tr.append($('<td>' + data[i].mostRecentCycle + '</td>'));
-                    tr.append($('<td>' + data[i].deploymentDate + '</td>'));
+                    tr.append($('<td>' + data[i].placementDate + '</td>'));
                     tr.append($('<td>' + data[i].city + '</td>'));
                     tr.append($('<td>' + data[i].country + '</td>'));
+
+                    $("#cycles-tbody").append(tr);
                 }
+
+                // initialize DataTables jQuery library
+                $("#devices").DataTable();
             },
             error: function(error) {
                 console.log(error);
@@ -32,14 +37,5 @@ $(function() {
         });
     }
 
-    function getSpecificDevice() {
-        $.get(GET_DEVICE_DETAILS, function(data) {
-            console.log(data);
-        }).then(function() {
-
-        });
-    }
-
     getAllDevices();
-    // getSpecificDevice();
 });
